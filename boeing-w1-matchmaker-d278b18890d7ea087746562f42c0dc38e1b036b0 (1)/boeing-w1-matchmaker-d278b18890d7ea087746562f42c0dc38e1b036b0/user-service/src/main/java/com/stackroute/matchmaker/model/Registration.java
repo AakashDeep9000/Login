cb9fdcdee1838faa38fc6b4960ef143572f97fc9
AakashDeep9000 <draakash9000@gmail.com>
2018-08-13 12:34:10 +0530

    initial commit

---
+++ boeing-w1-matchmaker-d278b18890d7ea087746562f42c0dc38e1b036b0 (1)/boeing-w1-matchmaker-d278b18890d7ea087746562f42c0dc38e1b036b0/user-service/src/main/java/com/stackroute/matchmaker/model/Registration.java
@@ -0,0 +1,44 @@
+package com.stackroute.matchmaker.model;
+
+public class Registration {
+
+	private String email;
+	private String password;
+	private String userName;
+	
+	public Registration() {
+		
+	}
+	
+	public Registration(String email, String password, String userName) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.userName = userName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String name) {
+		this.userName = name;
+	}
+	@Override
+	public String toString() {
+		return "Registration [email=" + email + ", password=" + password + ", userName=" + userName + "]";
+	}
+	
+	
+}
---
+++ boeing-w1-matchmaker-d278b18890d7ea087746562f42c0dc38e1b036b0 (1)/boeing-w1-matchmaker-d278b18890d7ea087746562f42c0dc38e1b036b0/user-service/src/main/java/com/stackroute/matchmaker/service/RegisterUser.java
@@ -0,0 +1,5 @@
+package com.stackroute.matchmaker.service;
+
+public class RegisterUser {
+
+}
